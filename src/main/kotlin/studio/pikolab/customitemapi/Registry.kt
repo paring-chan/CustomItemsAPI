2ebf16be71f9a5d074f68323530b17f51c82e169
parangee <parangee9706@gmail.com>
2020-07-30 12:32:15 +0900

    patch registry

--- src/main/kotlin/studio/pikolab/customitemapi/Registry.kt
+++ src/main/kotlin/studio/pikolab/customitemapi/Registry.kt
@@ -2,9 +2,11 @@ package studio.pikolab.customitemapi
 
 import studio.pikolab.customitemapi.classes.CustomItem
 
-object Registry {
-    val customItems = HashMap<String, CustomItem>()
-    fun registerItem(item: CustomItem) {
-        customItems[item.memberName] = item
+class Registry {
+    companion object {
+        val customItems = HashMap<String, CustomItem>()
+        fun registerItem(item: CustomItem) {
+            customItems[item.memberName] = item
+        }
     }
 }
